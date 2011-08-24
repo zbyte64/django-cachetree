890aaac86dcd3ddf8c4c38fc30c254eb5c82e537
Brian Jay Stanley <brianjaystanley@gmail.com>
2011-08-24 16:23:20 -0400

    tweaks to documentation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ version = '0.1.0'
 
 setup(name='django-cachetree',
       version=version,
-      description="caching of configurable trees of related model instances in Django",
+      description="caches configurable trees of related model instances in Django",
       long_description=open('README.rst').read(),
       classifiers=[
           'Development Status :: 4 - Beta',
